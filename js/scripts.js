d187c335c70667b2b0269baf52b32593f66f329f
John-Dig <rohnfiggins@gmail.com>
2023-01-20 10:17:18 -0800

    .501 passed

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 //Business Logic
 
 function robo(numb){
-  let roboTalk = [0, 'Beep'];
+  let roboTalk = [0, 'Beep', 'Boop!', "'Won't you be my neighbor?'", 4, 5,'We all have different gifts!', 7, "'I don't wear sweaters, I'm a robot'", 9];
   roboTalkS = roboTalk.toString();
-  numb = roboTalkS[0] + " " + roboTalk[1];
+  numb = roboTalkS
   return numb
 }
 
