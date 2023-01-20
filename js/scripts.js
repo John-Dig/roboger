282ef98537d1c7408bc94e7ae2899642b1b1bb15
John-Dig <rohnfiggins@gmail.com>
2023-01-20 11:56:52 -0800

    .71 passed

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,10 @@
 function robo(numb){
   //create original array, inefficient for 10 digits but doing this way to demonstrate looping and for further expansion to 100+ characters.
   originalArray = [];
-  for (i = 0; i < 10; i++) {
-    originalArray[i] = i
+  for (i = 0; i < numb; i++) {
+    originalArray[i] =  i % 10 
     };
+    console.log(originalArray);
   let roboTalk = [0, 'Beep', 'Boop!', "'Won't you be my neighbor?'", 4, 5,'We all have different gifts!', 7, "'I don't wear sweaters, I'm a robot'", 9];
   //transfer roboTalk into OriginalArray creating newArray, adding branching
   let roboTalkComplete = [];
@@ -24,5 +25,5 @@ function robo(numb){
   return roboTalkCompleteS;
 }
 
-//robo(1);
-console.log(robo(9));
+robo(150);
+//console.log(robo(150));
