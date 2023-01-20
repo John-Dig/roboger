375e9974b49fffcb7b97aea09d2b2e617e69e696
John-Dig <rohnfiggins@gmail.com>
2023-01-20 13:22:38 -0800

    .802 passed

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
 //Business Logic
 
 function robo(numb){
-  //create original array, inefficient for 10 digits but doing this way to demonstrate looping and for further expansion to 100+ characters.
+  //create original counting array
   originalArray = [];
   for (i = 0; i < numb; i++) {
     originalArray[i] =  i % 10 
     };
-    console.log(originalArray);
   let roboTalk = [0, 'Beep', 'Boop!', "'Won't you be my neighbor?'", 4, 5,'We all have different gifts!', 7, "'I don't wear sweaters, I'm a robot'", 9];
   //transfer roboTalk into OriginalArray creating newArray, adding branching
   let roboTalkComplete = [];
-  for (i = 0; i < roboTalk.length; i ++) {
-    if (originalArray[i] !== roboTalk[i]) {
-      roboTalkComplete[i] = roboTalk[i];
+  for (i = 0; i < numb; i ++) {
+    if (originalArray[i % 10] !== roboTalk[i % 10]) {
+      roboTalkComplete[i] = roboTalk[i % 10];
+      console.log (i % 10);
     }
-    else if (originalArray[i] === roboTalk[i]) {
-      roboTalkComplete[i] = originalArray[i];
+    else if (originalArray[i % 10] === roboTalk[i % 10]) {
+      roboTalkComplete[i] = originalArray[i % 10];
+      
+      console.log (i % 10);
     }
     else {
       window.alert("Error in branching")
@@ -24,6 +26,13 @@ function robo(numb){
   roboTalkCompleteS = roboTalkComplete.toString();
   return roboTalkCompleteS;
 }
+//document.getElementById("outputArea").innerText = wordCensor(document.getElementById("text-passage").value);
 
-robo(150);
-//console.log(robo(150));
+//robo(15);
+robo(15);
+
+// UI Logic
+window.addEventListener("load", function(e){
+  e.preventDefault();
+  document.getElementById("output").innerText = roboTalkCompleteS
+});
