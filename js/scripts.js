027e459a8535b124cd19d319370cc3d0068fee48
John-Dig <rohnfiggins@gmail.com>
2023-01-20 10:36:44 -0800

    .511 passed

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 //Business Logic
 
 function robo(numb){
+  //create original array, inefficient for 10 digits but doing this way to demonstrate looping and for further expansion to 100+ characters.
+  originalArray = [];
+  for (i = 0; i < 10; i++) {
+    originalArray[i] = i
+    };
+  console.log(originalArray);  
   let roboTalk = [0, 'Beep', 'Boop!', "'Won't you be my neighbor?'", 4, 5,'We all have different gifts!', 7, "'I don't wear sweaters, I'm a robot'", 9];
-  roboTalkS = roboTalk.toString();
-  numb = roboTalkS
-  return numb
+  for (i = 0; i < roboTalk.length; i ++) {
+    let roboTalkComplete = [];
+    //roboTalkComplete[i] = 
+  };
+  roboTalkCompleteS = roboTalk.toString();
+  numb = roboTalkCompleteS;
+  return numb;
 }
 
-
-
-
-console.log(robo(1));
+robo(1);
+//console.log(robo(1));
