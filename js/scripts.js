ec0555aafdae648fa9b3734941bd741dc8135f5d
John-Dig <rohnfiggins@gmail.com>
2023-01-20 16:59:13 -0800

    wip, still stuck at .95

--- js/scripts.js
+++ js/scripts.js
@@ -8,62 +8,68 @@ function robo(numb){
   };
   //roboTemp
   let roboTemp = [0, 'Beep', 'Boop!', "'Won't you be my neighbor?'", 4, 5,'We all have different gifts!', 7, "'I don't wear sweaters, I'm a robot'", 9];
-  //roboTemp past 9
+  //roboTemp past 9 works
   if (numb > 9) {
     for (i = 10; i < numb; i ++){
       if (typeof(roboTemp[i-10]) == 'string'){ 
-        console.log("worked");
         roboTemp[i] = roboTemp[i % 10];
       }
-        
         else {  
           roboTemp[i] = counterArray[i]
         }
     }
-    console.log(roboTemp);
   }
   //transfer roboTemp into OriginalArray creating newArray, adding branching
-  
-  //console.log(toString(roboTemp[7]).includes(7))
+  let a = roboTemp[10].toString();
+  let aS = a.toString();
+  console.log(a);
+  console.log(roboTemp[10]);
+  console.log(toString(roboTemp[9]).includes(''));
   //console.log(counterArray[12].toString());
   
   let roboTalkFinal = [];
-    for (i = 0; i < numb; i ++) {
-    //after 9, must switch individual iteration to string
-    if (i > 9)  {
-          //roboTalkFinal[i] = roboTemp[i].toString();
-      //roboTemp[i] =
-      //console.log(typeof(roboTalkFinal[i]))
-      if (true === true) {
-        //roboTalkFinal[i] = roboTemp[1];
-      }
-      else if (roboTalkFinal[i].includes(2)) {
-        roboTalkFinal[i] = roboTemp[2]
-      }
-      else if (roboTalkFinal[i].includes(3)) {
-        roboTalkFinal[i] = roboTemp[3]
-      }
-      else if (roboTalkFinal[i].includes(6)) {
-        roboTalkFinal[i] = roboTemp[6]
-      }
-      else if (roboTalkFinal[i].includes(8)) {
-        roboTalkFinal[i] = roboTemp[8]
-      }
-      //else window.alert("error in nested branch")
+  for (i = 0; i < numb; i ++) {
+    if (i <= 9) {
+      roboTalkFinal[i] = roboTemp[i];    
     }
-    else if (counterArray[i % 10] !== roboTemp[i % 10]) {
-      roboTalkFinal[i] = roboTemp[i % 10];
-    }
-    else if (counterArray[i % 10] === roboTemp[i % 10]) {
-      roboTalkFinal[i] = counterArray[i % 10];
-    }
-    else {
-      window.alert("Error in branching")
+    else if (i > 9)  {
+      let a1 = roboTemp[1];
+      let a2 = roboTemp[2];
+      let a3 = roboTemp[3];
+      let a6 = roboTemp[6];
+      let a8 = roboTemp[8];
+      console.log(a1, a2, a3, a6, a8);
+
+      if (roboTemp[i] == 'number') {
+
+          roboTalkFinal[i] = a1
+          console.log("here");
+        }
+        else if (toString(counterArray[i]).includes(3)) {
+          roboTalkFinal[i] = roboTemp[3]
+        }
+        else if (toString(counterArray[i]).includes(6)) {
+          roboTalkFinal[i] = roboTemp[6]
+        }
+        else if (toString(counterArray[i]).includes(8)) {
+          roboTalkFinal[i] = roboTemp[8]
+        }
+        else {
+          console.log("Elsed out")
+        }
     };
-  };
-  roboTalkCompleteS = roboTalkFinal.toString();
-  return roboTalkCompleteS;
-}
+  }
+    // else if (counterArray[i % 10] !== roboTemp[i % 10]) {
+      //   roboTalkFinal[i] = roboTemp[i % 10];
+      //   console.log("here");
+      // }
+      // else if (counterArray[i % 10] === roboTemp[i % 10]) {
+        //   roboTalkFinal[i] = counterArray[i % 10];
+        //   console.log(" or here here");
+        // }
+        roboTalkCompleteS = roboTalkFinal.toString();
+        
+      };
 //document.getElementById("outputArea").innerText = wordCensor(document.getElementById("text-passage").value);
 
 robo(20);
