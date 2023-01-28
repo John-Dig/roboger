e3959ffcdf3e73588ab051b646d7680c99a2c39b
John-Dig <rohnfiggins@gmail.com>
2023-01-28 11:01:29 -0800

    1.21, minor fix to declaring variables, removed console.log's

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 //Business Logic
 function robo(numb){
   //counting array
-  counterArray = [];
-  for (i = 0; i < numb; i++) {
+  let counterArray = [];
+  for (let i = 0; i < numb; i++) {
     counterArray[i] =  i;
   };
   //roboTemp
   let roboTemp = [0, 'Beep', 'Boop!', "'Won't you be my neighbor?'", 4, 5,"'We all have different gifts!'", 7, "'I don't wear sweaters, I'm a robot ¯\_(ツ)_/¯'", 9];
   //roboTemp past 9
   if (numb > 9) {
-    for (i = 10; i < numb; i ++){
+    for (let i = 10; i < numb; i ++){
       if (typeof(roboTemp[i-10]) == 'string'){ 
         roboTemp[i] = roboTemp[i % 10];
       }
@@ -19,47 +19,44 @@ function robo(numb){
     }
   }
   let roboTalkFinal = [];
-  for (i = 0; i < numb; i ++) {
-  let rTS = (roboTemp[i]).toString();
-  // less than 9
-  if (i <= 9) {
-    roboTalkFinal[i] = roboTemp[i];    
-  }
-  // more than 9
-  else if (i > 9) {
-    if (rTS.includes(1)) {
-      roboTalkFinal[i] = roboTemp[1];
+  for (let i = 0; i < numb; i ++) {
+    let rTS = (roboTemp[i]).toString();
+    // less than 9
+    if (i <= 9) {
+      roboTalkFinal[i] = roboTemp[i];    
     }
-    else if (rTS.includes(2)) {
-      roboTalkFinal[i] = roboTemp[3]
-    }
-    else if (rTS.includes(3)) {
-      roboTalkFinal[i] = roboTemp[3]
-    }
-    else if (rTS.includes(6)) {
-      roboTalkFinal[i] = roboTemp[6]
-    }
-    else if (rTS.includes(8)) {
-      ;
-    }
-    else {
-      roboTalkFinal[i] = roboTemp[i]
+    // more than 9
+    else if (i > 9) {
+      if (rTS.includes(1)) {
+        roboTalkFinal[i] = roboTemp[1];
+      }
+      else if (rTS.includes(2)) {
+        roboTalkFinal[i] = roboTemp[3]
+      }
+      else if (rTS.includes(3)) {
+        roboTalkFinal[i] = roboTemp[3]
+      }
+      else if (rTS.includes(6)) {
+        roboTalkFinal[i] = roboTemp[6]
+      }
+      else if (rTS.includes(8)) {
+        ;
+      }
+      else {
+        roboTalkFinal[i] = roboTemp[i]
+      };
     };
-  };
-}
+  }
 return roboTalkFinal;
 }  
 
 //reset procedure
 function resetProcedure(){
   let outputArea = document.getElementById("output-area");
-  console.log(outputArea);
   outputArea.innerText = ""
-  console.log(document.querySelector("p"));
   document.getElementById("reset-button").setAttribute("class","hidden");
   document.getElementById("input-area").value = ""
   clearInterval(timer);
-
 }
 //timing logic
 function timingOutput(msT){
@@ -69,14 +66,13 @@ function timingOutput(msT){
   timer = setInterval(() => {
     //if no input 
     if (argument === "") {
-      console.log(parseInt(argument));
       outputArea.append("Mr. Roboger needs INPUT, please push reset and give him a number");
       document.getElementById("reset-button").setAttribute("class","visible");
       clearInterval(timer);
     }
     //printing sequence
     let outputArray = robo(argument);
-    outputString = outputArray.toString();
+    const outputString = outputArray.toString();
     outputArea.append(outputString.charAt(i));
     if (i === 0){
       document.getElementById("reset-button").setAttribute("class","visible");
@@ -84,7 +80,6 @@ function timingOutput(msT){
     i++;
     //stopping sequence
     if (outputString.length === i && argument !== "") {
-      console.log(outputString.length);
       clearInterval(timer);
     }
   }, msT);
